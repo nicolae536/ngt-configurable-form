6e79b8d49e00963aeba7148f50894e42a0aed177
Unknown <nicolae536@gmail.com>
2017-10-24 23:13:28 +0300

    Fix reaveal.js progress and history

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -26,12 +26,13 @@ export class PresentationComponent implements AfterViewInit, OnDestroy {
                 transition: 'slide', // none/fade/slide/convex/concave/zoom
                 // More info https://github.com/hakimel/reveal.js#dependencies
                 dependencies: [
-                    { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-                    { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-                    { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-                    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-                    { src: 'plugin/zoom-js/zoom.js', async: true },
-                    { src: 'plugin/notes/notes.js', async: true }
+                    // not working right now
+                    // { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+                    // { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+                    // { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+                    // { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+                    // { src: 'plugin/zoom-js/zoom.js', async: true },
+                    // { src: 'plugin/notes/notes.js', async: true }
                 ]
             });
         };
